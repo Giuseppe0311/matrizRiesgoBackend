537a3276cab35a486675c4170edab75a995c2005
Angel Giuseppe Barrera Romero <barrerita_romero@hotmail.com>
2024-10-03 09:05:43 -0500

    se implemento seguridad con roles y token

--- src/main/java/com/seguridad/matriz/config/SecurityConfig.java
+++ src/main/java/com/seguridad/matriz/config/SecurityConfig.java
@@ -1,26 +1,52 @@
 package com.seguridad.matriz.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permitir todas las rutas
-                .allowedOrigins("*") // Permitir cualquier origen
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Métodos permitidos
-                .allowedHeaders("authorization", "Content-Type", "x-auth-token", "Access-Control-Allow-Origin") // Headers permitidos
-                .exposedHeaders("Content-Disposition") // Headers expuestos
-                .allowCredentials(false) // No se permiten credenciales
-                .maxAge(3600); // Tiempo en segundos que el navegador puede cachear la respuesta de preflight
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();  // Permitir acceso a Swagger
+                    http.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "Content-Type", "x-auth-token","Access-Control-Allow-Origin"));
+        configuration.setExposedHeaders(List.of("Content-Disposition"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/seguridad/matriz/rest/CrudController.java
+++ src/main/java/com/seguridad/matriz/rest/CrudController.java
@@ -2,6 +2,7 @@ package com.seguridad.matriz.rest;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +17,23 @@ import java.util.Map;
 public interface CrudController<C, U, V> {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('admin_client','user_client')")
     List<V> getAll();
 
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasAnyRole('admin_client','user_client')")
     V getById(@PathVariable(required = false) Map<String, String> idMap);
 
     @PostMapping
+    @PreAuthorize("hasRole('admin_client')")
     @ResponseStatus(HttpStatus.CREATED)
     V create(@Valid @RequestBody C createDTO);
 
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('admin_client')")
     V update(@Valid @RequestBody U updateDTO, @PathVariable(required = false) Map<String, String> idMap);
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('admin_client')")
     void delete(@PathVariable(required = false) Map<String, String> idMap);
 }
